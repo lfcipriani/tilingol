640b3113efb6020f444ba935489e44629796f01d
Luis Cipriani <lcipriani@twitter.com>
2014-05-31 16:45:34 -0300

    fixing PWM

--- servo.py
+++ servo.py
@@ -1,30 +1,21 @@
-# Servo Control
+import RPi.GPIO as GPIO
 import time
 
-def set(property, value):
-	try:
-		f = open("/sys/class/rpi-pwm/pwm0/" + property, 'w')
-		f.write(value)
-		f.close()	
-	except:
-		print("Error writing to: " + property + " value: " + value)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(18, GPIO.OUT)
+
+p = GPIO.PWM(18, 50)
+
+p.start(0)
+
+while 1:
+    for dc in range(0, 101, 5):
+        p.ChangeDutyCycle(dc)
+        time.sleep(0.1)
+    for dc in range(100, -1, -5):
+        p.ChangeDutyCycle(dc)
+        time.sleep(0.1)
  
- 
-def setServo(angle):
-	set("servo", str(angle))
-	
-		
-set("delayed", "0")
-set("mode", "servo")
-set("servo_max", "180")
-set("active", "1")
- 
-delay_period = 0.01
- 
-while True:
-	for angle in range(0, 180):
-		setServo(angle)
-		time.sleep(delay_period)
-	for angle in range(0, 180):
-		setServo(180 - angle)
-		time.sleep(delay_period)
+p.stop()
+
+GPIO.cleanup()
