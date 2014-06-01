77f283f2ddf8cf269b9d0b2b9c78d9ede08b5a62
Luis Cipriani <lcipriani@twitter.com>
2014-06-01 10:53:41 -0300

    first version of tilingol working

--- servo.py
+++ servo.py
@@ -4,20 +4,15 @@ import time
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(18, GPIO.OUT)
 
-p = GPIO.PWM(18, 50)
+p = GPIO.PWM(18, 100)
 
-p.start(0)
+p.start(19)
 
-#while 1:
-    print("vai")
-    for dc in range(0, 101, 5):
-        p.ChangeDutyCycle(dc)
-        print(dc)
+time.sleep(2)
+while 1:
+        p.ChangeDutyCycle(16)
         time.sleep(0.1)
-    print("volta")
-    for dc in range(100, -1, -5):
-        p.ChangeDutyCycle(dc)
-        print(dc)
+        p.ChangeDutyCycle(19)
         time.sleep(0.1)
  
 p.stop()
