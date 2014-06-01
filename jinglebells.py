941542790e5338fa38400b63089bcf2fb8186647
Luis Cipriani <lcipriani@twitter.com>
2014-06-01 12:05:10 -0300

    fixing shake from jungle bells class

--- jinglebells.py
+++ jinglebells.py
@@ -1,23 +1,23 @@
 import RPi.GPIO as GPIO
 import time
 
-class JingueBells:
+class JingleBells:
 
     def __init__(self, pin):
         self.pin = pin
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(self.pin, GPIO.OUT)
-        self.pwm = GPIO.PWM(self.pin, 100)
 
     def shake(self, iterations, speed):
+        self.pwm = GPIO.PWM(self.pin, 100)
         self.pwm.start(19)
         time.sleep(speed)
         for n in range(iterations):
-            pwm.ChangeDutyCycle(16)
+            self.pwm.ChangeDutyCycle(16)
             time.sleep(speed)
-            pwm.ChangeDutyCycle(19)
+            self.pwm.ChangeDutyCycle(19)
             time.sleep(speed)
-        pwm.stop()
+        self.pwm.stop()
 
     def __del__(self):
         GPIO.cleanup()
