8b153ab67b97b6f1a700c55507fbd315b9f62574
Luis Cipriani <lcipriani@twitter.com>
2014-06-06 13:35:40 -0300

    adding hashtag to test the bell

--- goalstream.py
+++ goalstream.py
@@ -20,20 +20,23 @@ class GoalStream(TwythonStreamer):
         print("\\o/ ring! ring! \\o/")
         bell.shake(iterations, speed)
 
+    def easteregg(self, data):
+        return ("toqueosinopequenino" == data["entities"]["hashtags"][0]["text"])
+
     def on_success(self, data):
         if 'text' in data:
             self.peak_detector.collect_frequency()
 
-            if self.peak_detector.is_this_a_peak():
+            if self.peak_detector.is_this_a_peak() or self.easteregg(data):
                 print "UEPA"
                 if not self.debug:
                     if not self.ringThread.isAlive():
                         self.ringThread = threading.Thread(target = self.ring_the_bells, args=(self.ring, SHAKES, SPEED,))
                         self.ringThread.start()
 
+            print data
             #print "  >> @" + data['user']['screen_name'].encode('utf-8') + ": " + data['text'].encode('utf-8')
 
     def on_error(self, status_code, data):
         print "Erro: " + str(status_code) + str(data)
-
 
